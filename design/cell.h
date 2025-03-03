fa2b52454539d6a28774f5e32c64ae4aea57832f
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-03-03 23:41:24 +0300

    Update cell.h

--- design/cell.h
+++ design/cell.h
@@ -3,21 +3,53 @@
 #include "common.h"
 #include "formula.h"
 
+#include <unordered_set>
+
+class Impl {
+public:
+    virtual ~Impl();
+    virtual std::string GetText() = 0;
+    virtual CellInterface::Value GetValue() = 0;
+};
+
+class EmptyImpl : public Impl {
+public:
+    std::string GetText() override;
+    CellInterface::Value GetValue();
+};
+
+class TextImpl : public Impl {
+public:
+    TextImpl(std::string text);
+    std::string GetText() override;
+    CellInterface::Value GetValue();
+private:
+    std::string text_;
+};
+
+class FormulaImpl : public Impl {
+public:
+    FormulaImpl(std::string text);
+    std::string GetText() override;
+    CellInterface::Value GetValue();
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+};
+
+
 class Cell : public CellInterface {
 public:
     Cell();
-    ~Cell();
-
+    ~Cell() = default;
     void Set(std::string text);
     void Clear();
-
     Value GetValue() const override;
     std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_reference;
+    std::unordered_set<Cell*> dependent_cell;
+    std::unordered_set<Cell*> which_depends_on;
 };

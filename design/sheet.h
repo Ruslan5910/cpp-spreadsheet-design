4085668f6af3eb3b71625bf1e5fdca136850de8a
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-03-04 01:31:02 +0300

    Update sheet.h

--- design/sheet.h
+++ design/sheet.h
@@ -21,14 +21,6 @@ public:
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
-
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
-
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+	std::vector<std::vector<std::unique_ptr<Cell>>> sheet_;
 };

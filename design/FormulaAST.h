a616c1aeaf7724d53811392438f897219353c471
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-03-03 20:37:31 +0300

    Update FormulaAST.h

--- design/FormulaAST.h
+++ design/FormulaAST.h
@@ -17,17 +17,19 @@ class ParsingError : public std::runtime_error {
 
 class FormulaAST {
 public:
-    explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr);
+    explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr, std::forward_list<Position> cells_);
     FormulaAST(FormulaAST&&) = default;
     FormulaAST& operator=(FormulaAST&&) = default;
     ~FormulaAST();
 
-    double Execute() const;
+    double Execute(const std::function<double>(Position)& get_val_func) const;
     void Print(std::ostream& out) const;
     void PrintFormula(std::ostream& out) const;
+    const std::forward_list<Position> GetCells const;
 
 private:
     std::unique_ptr<ASTImpl::Expr> root_expr_;
+    std::forward_list<Position> cells_;
 };
 
 FormulaAST ParseFormulaAST(std::istream& in);
